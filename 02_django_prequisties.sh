c1f00b9a0c7a5d80f77ea1402db722905c456f55
Nagaraj Sajjan <sajjan.nagaraj@gmail.com>
2021-12-09 18:33:05 +0530

    Update 02_django_prequisties.sh

--- 02_django_prequisties.sh
+++ 02_django_prequisties.sh
@@ -9,3 +9,17 @@ echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
 echo "export WORKON_HOME=~/Env" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 source ~/.bashrc
+
+echo "export OALPCSS_ENV='PROD'" >> ~/.bashrc
+echo "export OALPCSS_SECRET_KEY='^9ia*eb$26dqcl(ms39d^sy^ihfhq^^sml1o$srlhln5r#e8t('" >> ~/.bashrc
+echo "export OALPCSS_DB_NAME='oalpcss'" >> ~/.bashrc
+echo "export OALPCSS_DB_USER='oalpcss_root'" >> ~/.bashrc
+echo "export OALPCSS_DB_PASSWORD='8ae23f226a216dc08ac9001cdacd1c23'" >> ~/.bashrc
+echo "export OALPCSS_DB_HOST='localhost'" >> ~/.bashrc
+echo "export OALPCSS_ALLOWED_HOSTS='website_url'" >> ~/.bashrc
+source ~/.bashrc
+
+mkvirtualenv oalp-css --python=python3
+workon oalp-css
+
+cd /var/www/html/
