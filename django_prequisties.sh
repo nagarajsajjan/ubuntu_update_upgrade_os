f96abad22eed14edb7e35a3b128874438a1b04e1
Nagaraj Sajjan <sajjan.nagaraj@gmail.com>
2021-12-09 13:46:52 +0530

    Update django_prequisties.sh

--- django_prequisties.sh
+++ django_prequisties.sh
@@ -1,10 +1,9 @@
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
+sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl
+
 sudo -H pip3 install virtualenv virtualenvwrapper
 
 sudo mkdir -p /etc/uwsgi/sites
 sudo -H pip3 install uwsgi
-sudo apt-get install nginx
 
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
 echo "export WORKON_HOME=~/Env" >> ~/.bashrc
